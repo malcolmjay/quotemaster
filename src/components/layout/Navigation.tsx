5a14bd907bd83e8021ac88703dd42a8976158f23
malcolmjay <35127786+malcolmjay@users.noreply.github.com>
2025-12-12 15:26:35 -0600

    Updated Navigation.tsx

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -52,7 +52,7 @@ export const Navigation: React.FC<NavigationProps> = ({ activeTab, onTabChange }
       {/* Navigation Menu */}
       <nav className={`
         fixed top-0 left-0 h-full w-64
-        bg-white dark:bg-gray-800 border-r border-gray-200 dark:border-gray-700 z-40 transform transition-transform duration-300 ease-in-out
+        bg-white dark:bg-gray-800 border-r border-gray-200 dark:border-gray-700 z-50 transform transition-transform duration-300 ease-in-out
         ${isOpen ? 'translate-x-0' : '-translate-x-full'}
       `}>
         <div className="p-3 pt-16">
